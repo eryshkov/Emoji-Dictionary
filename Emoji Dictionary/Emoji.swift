dfbff4ed931b85339b3e290f42e69c6fbb71a5c0
eryshkov <wendor265>
2018-10-08 21:37:21 +0300

    init commit

---
+++ Emoji Dictionary/Emoji.swift
@@ -0,0 +1,23 @@
+//
+//  Emoji.swift
+//  Emoji Dictionary
+//
+//  Created by Evgeniy Ryshkov on 08/10/2018.
+//  Copyright © 2018 Evgeniy Ryshkov. All rights reserved.
+//
+
+import Foundation
+
+class Emoji {
+    var symbol: String
+    var name: String
+    var description: String
+    var usage: String
+    
+    init(symbol: String, name: String, description: String, usage: String) {
+        self.symbol = symbol
+        self.name = name
+        self.description = description
+        self.usage = usage
+    }
+}
